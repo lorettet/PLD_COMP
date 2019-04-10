933f34a518d70a359b3bb73e81b3a4dadfc96c82
Théo Lorette-Froidevaux <theo@rtone.fr>
2019-04-10 08:05:55 +0200

    Ajout de tests

---
+++ testfiles/ifStatement/ifElseImbriq.c
@@ -0,0 +1,12 @@
+int main() {
+	int a = 4;
+	if((a+1) == 5) {
+	 if(a+2 >0)
+	 {
+		 a = 52;
+	 }
+	} else {
+	return 3;
+	}
+	return a;
+}
---
+++ testfiles/realProgram/Ifcomplexe.c
@@ -0,0 +1,20 @@
+
+int main()
+{
+  int a=12;
+  int b = 6;
+  int c;
+  if(a <= b*2) {
+    if(a >= 12) {
+      c = 9;
+      c = somme(c,a);
+    } else {
+      int c = 12;
+    }
+  }
+  return c;
+}
+
+int somme(int a, int b){
+  return a+b;
+}
